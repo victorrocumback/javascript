a0d2672b8bafa9e4074e3326afe2ded5fc802fa2
Victor Rocumback <117930710+victorrocumback@users.noreply.github.com>
2023-04-29 18:36:32 -0300

    Cursos separados

---
+++ CursoEmVideo/exercicios/ex018/script.js
@@ -0,0 +1,8 @@
+let student = {
+    name: "victor",
+    age: 22,
+    weight: 66.2
+};
+
+console.log(`${student.name} tem ${student.age} anos e ${student.weight}Kg.`)
+console.log(typeof student)
